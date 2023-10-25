5451239d46a9cf995d4c19c5f3a5057826404277
gamza2695 <gamza2695@gmail.com>
2023-10-25 16:06:48 +0900

    1025 수업

--- 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/admin/model/service/AjaxServiceImpl.java
+++ 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/admin/model/service/AjaxServiceImpl.java
@@ -1,18 +1,28 @@
 package edu.kh.project.admin.model.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import edu.kh.project.admin.model.mapper.AjaxMapper;
 import edu.kh.project.member.model.dto.Member;
+import edu.kh.project.member.model.mapper.MemberMapper;
 
 @Service
 public class AjaxServiceImpl implements AjaxService{
 
 	@Autowired
 	private AjaxMapper mapper;
+	
+	@Autowired
+	private BCryptPasswordEncoder bcrypt; // 암호화
+	
+	@Autowired
+	private MemberMapper memberMapper; // 회원 Mapper -> 있는거 사용하기
+
 	
 	
 	@Override
@@ -37,4 +47,32 @@ public class AjaxServiceImpl implements AjaxService{
 	public List<String> selectEmailList(String keyword) {
 		return mapper.selectEmailList(keyword);
 	}
+	
+	@Override
+	public List<Member> selectAll() {
+		return mapper.selectAll();
+	}
+	
+	
+	// 샘플 계정 가입
+	@Override
+	public int insertMember(Member member) {
+		
+		// 비밀번호 "1234"를 암호화해서 member에 추가
+		member.setMemberPw(bcrypt.encode("1234"));
+		
+		return memberMapper.signup(member);
+	}
+	
+	
+	// 회원 탈퇴여부 변경
+	@Override
+	public int updateFlag(Map<String, Object> paramMap) {
+		return mapper.updateFlag(paramMap);
+	}
+	
+	
+	
 }
+
+
