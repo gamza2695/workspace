b99c3ed43527bd5c20236887ccff3200eb5a2a33
gamza2695 <gamza2695@gmail.com>
2023-10-30 17:37:43 +0900

    1030 수업 - 통합구현 TEST

---
+++ 06_Framework/SpringTest2/Tonghab1/src/main/java/com/kh/test/board/controller/BoardController.java
@@ -0,0 +1,42 @@
+package com.kh.test.board.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.kh.test.board.model.dto.Board;
+import com.kh.test.board.model.service.BoardService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class BoardController {
+
+	@Autowired
+	private BoardService service;
+	
+	@GetMapping("searchBoard")
+	public String searchBoard(String inputTitle, Model model) {
+		
+		List<Board> resultBoard = new ArrayList<>();
+		resultBoard = service.searchBoard(inputTitle);
+		
+		
+		if(resultBoard != null) {
+			model.addAttribute("resultBoard",resultBoard);
+			return "searchSuccess";
+		}
+		
+		
+		return "searchFail";
+		
+		
+	}
+	
+	
+}
